500394ca395403df7c4adf32a4c355af87b3b942
dustindemmerle <dustindemmerle@gmail.com>
2021-11-05 16:24:25 +0100

    Neu: Scanner

--- app/src/main/java/com/example/a3gcheckapp/MainActivity.java
+++ app/src/main/java/com/example/a3gcheckapp/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.a3gcheckapp;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +27,10 @@ public class MainActivity extends AppCompatActivity {
 
         scanPageButton = (ImageButton) findViewById(R.id.ZertifikatPruefButton);
         scanPageButton.setOnClickListener(v -> openScanPage());
-    }
+        };
+
+
+
     public void openInformation(){
         Intent intent = new Intent(this, information.class);
         startActivity(intent);
@@ -31,3 +40,4 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 }
+
--- app/src/main/java/com/example/a3gcheckapp/scan.java
+++ app/src/main/java/com/example/a3gcheckapp/scan.java
@@ -1,14 +1,23 @@
 package com.example.a3gcheckapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class scan extends AppCompatActivity {
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+public class scan extends AppCompatActivity  {
 
     private ImageButton backButton;
+    private CodeScanner mCodeScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +26,42 @@ public class scan extends AppCompatActivity {
 
         backButton = (ImageButton) findViewById(R.id.backButton);
         backButton.setOnClickListener(v -> openMain());
+
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(scan.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCodeScanner.startPreview();
+            }
+        });
+
     }
     public void openMain(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause(){
+        mCodeScanner.releaseResources();
+        super.onPause();
     }
 }
