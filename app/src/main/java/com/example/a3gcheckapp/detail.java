d157647888106f124a7eff20dd04e06696594bc1
dustindemmerle <dustindemmerle@gmail.com>
2021-12-07 23:47:27 +0100

    Update: DatailPage

--- app/src/main/java/com/example/a3gcheckapp/detail.java
+++ app/src/main/java/com/example/a3gcheckapp/detail.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 //The class detail generates the detailed view of the scanned certificates.
 public class detail extends AppCompatActivity {
 
     private ImageButton backButton;
+    private ImageView trashIcon;
 
     @Override
     // The method generates the page including the detailed view of the scanned certificates with a back button.
@@ -20,10 +22,17 @@ public class detail extends AppCompatActivity {
         backButton = (ImageButton) findViewById(R.id.backButton);
         backButton.setOnClickListener(v -> openMain());
 
+        trashIcon = (ImageView) findViewById(R.id.trashIcon);
+        trashIcon.setOnClickListener(v -> deleteCertificate());
+
     }
     // The method opens the class MainActivity.
     public void openMain(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void deleteCertificate(){
+
     }
 }
